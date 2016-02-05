ebdffbe18b17d425ae503c3b0c7106bfc24cf71e
yeyezaw <zawyehtut@outlook.com>
2016-02-05 14:09:36 -0500

    Update - logo, font-family

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    //Slick plugin
+    $('.slides').slick({
+                slidesToShow: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                arrows: false,
+                dots: true,
+                pauseOnHover: false
+    });
+    
     $('.thumbnail').click(function(){
         
     });
